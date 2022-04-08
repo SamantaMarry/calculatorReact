f3a30c5ddd90525cade8ae65ba9dc92268dd3ccc
Samanta Marry <samantamaarry@gmail.com>
2022-04-07 22:01:38 -0300

    ajustes calculadora

--- src/components/calculator/index.tsx
+++ src/components/calculator/index.tsx
@@ -1,50 +1,90 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './style.scss';
 
 const Calculator = () => {
     const [num, setNum] = useState<string>('');
-    const [result, setResult] = useState()
+    const [dinamicResult, setDinamicResult] = useState<string>('')
+    const [result, setResult] = useState([])
+    const [history, setHistory] = useState([])
+    const [error, setError] = useState(false);
 
-/*     function inputNum(e){
         
+    function inputNum(e:any){
+        let input = e.target.value;
         
+        setNum(num + input);
+        
+        setDinamicResult(eval(num));
     }
- */
 
+    function clear(e: any){
+        setNum('');
+        setError(false)
+    } 
 
     function handleCalculate(expression: string){
-        setResult(eval(expression))
+        try {
+            let newResult = result as any;
+            const value = eval(expression);
+            newResult.push(value);
+            
+            let newHistory = history as any;
+            newHistory.push(expression + ' = ' + value);
+            setHistory(newHistory.slice(-5));
+    
+            setResult(newResult);
+            setNum('')
+        } catch (e) {
+            setError(true)
+        }
+    }
+
+    function handleOnChange(e: any){
+        let input = e.target.value;
+        setError(false)
+        setNum(input);
+    }
+
+    function handleKeyDown(e: any){        
+        if (e.key === 'Enter') {
+            handleCalculate(num)
+        }
     }
 
     return (
         <div>
-            {result && result.map()}
-            <p> {result} </p>
-            <input type="text" />
+            
+            {history.map(value => 
+                <p key={Math.random()}> {value} </p>
+            )} 
+
+            {num && <p>{dinamicResult}</p>}
+    
+            <input type='text' value={num} onChange={handleOnChange} onKeyDown={handleKeyDown}className="form-control"></input>                
+            {error && <p className="error"> expressao mal formada </p>}
             <div> 
-                <button>1</button>
-                <button>2</button>
-                <button>3</button>
-                <button>4</button>
-                <button>5</button>
-                <button>7</button>
-                <button>8</button>
-                <button>9</button>
-                <button>0</button>
-                <button onClick={() => handleCalculate('2 + 3 + 4 * 1')}>=</button>
+                <button onClick={inputNum} value={'1'}>1</button>
+                <button onClick={inputNum} value={'2'}>2</button>
+                <button onClick={inputNum} value={'3'}>3</button>
+                <button onClick={inputNum} value={'4'}>4</button>
+                <button onClick={inputNum} value={'5'}>5</button>
+                <button onClick={inputNum} value={'6'}>6</button>
+                <button onClick={inputNum} value={'7'}>7</button>
+                <button onClick={inputNum} value={'8'}>8</button>
+                <button onClick={inputNum} value={'9'}>9</button>
+                <button onClick={inputNum} value={'0'}>0</button>
+                <button onClick={() => handleCalculate(num)}>=</button>
                 <button>,</button>
             </div>
             <div> 
-                <button>+</button>
-                <button>-</button>
-                <button>*</button>                
-                <button>/</button>
+                <button onClick={clear}>AC</button>
+                <button onClick={inputNum} value={'+'}>+</button>
+                <button onClick={inputNum} value={'-'}>-</button>
+                <button onClick={inputNum} value={'*'}>*</button>                
+                <button onClick={inputNum} value={'/'}>/</button>
             </div>
-
-
         </div>  
     )
-
 }
 
 export default Calculator;
